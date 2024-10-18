730caf49ae85ab8fbb44a28ff2617a894a42bcfd
Leonard Laput <online.lorno@gmail.com>
2024-10-19 00:08:09 +0800

    ✨ Update favicon and page meta data

--- src/app/(pages)/symbol/[id]/page.tsx
+++ src/app/(pages)/symbol/[id]/page.tsx
@@ -1,4 +1,18 @@
+import type { Metadata } from "next";
+
 import SymbolDetails from "@/components/SymbolDetails";
+
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = params.id;
+
+  return {
+    title: `${id} - Clepher - Alphavantage API integration`,
+  };
+}
 
 export default function SymbolDetailsPage() {
   return <SymbolDetails />;
